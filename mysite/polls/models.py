04438e7c50207de4965f52004670a44067a92105
suuu0122 <suuu0122lab@gmail.com>
2023-07-09 21:12:38 +0900

    finish chapter07

--- mysite/polls/models.py
+++ mysite/polls/models.py
@@ -1,5 +1,6 @@
 import datetime
 
+from django.contrib import admin
 """https://docs.djangoproject.com/ja/4.2/ref/models/instances/#django.db.models.Model"""
 from django.db import models
 """https://docs.djangoproject.com/ja/4.2/ref/utils/#module-django.utils.timezone"""
@@ -17,7 +18,12 @@ class Question(models.Model):
     def __str__(self):
         return self.question_text
     
-
+    
+    @admin.display(
+        boolean=True,
+        ordering="pub_date",
+        description="Published recently?",
+    )
     def was_published_recently(self):
         now = timezone.now()
         return now - datetime.timedelta(days=1) <= self.pub_date <= now
